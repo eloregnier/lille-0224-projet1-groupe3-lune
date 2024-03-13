f26c309ab3d1b84674d6e26109db7ae34bc83a2b
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-03-13 16:18:10 +0100

    resolving conflit

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const navBarFixed = document.getElementById('navBarFixed');
 const body = document.body;
 
 window.onscroll = function () {
-    myFunction();
+	myFunction();
 }
 
 let sticky = navBarFixed.offsetTop;
 
 function myFunction() {
-    console.log(sticky)
-    if (window.pageYOffset >= sticky) {
-        navBarFixed.classList.add('sticky')
-    } else {
-        navBarFixed.classList.remove('sticky')
-    }
+	console.log(sticky)
+	if (window.pageYOffset >= sticky) {
+		navBarFixed.classList.add('sticky')
+	} else {
+		navBarFixed.classList.remove('sticky')
+	}
 }
 // fin nav bar fixed
