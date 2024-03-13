75118f4abc0151191121b938ce3195282fdce95e
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-03-13 15:59:07 +0100

    nav bar mobile finie

---
+++ script.js
@@ -0,0 +1,19 @@
+// nav bar fixed
+const navBarFixed = document.getElementById('navBarFixed');
+const body = document.body;
+
+window.onscroll = function () {
+    myFunction();
+}
+
+let sticky = navBarFixed.offsetTop;
+
+function myFunction() {
+    console.log(sticky)
+    if (window.pageYOffset >= sticky) {
+        navBarFixed.classList.add('sticky')
+    } else {
+        navBarFixed.classList.remove('sticky')
+    }
+}
+// fin nav bar fixed
