954e1261d552cf519fcc6e7d7c2e6e06c18a009c
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-03-20 17:15:10 +0100

    ajout des alt sur les images

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-
 // nav bar fixed
-const navBarFixed = document.getElementById('navBarFixed');
+const navBarFixed = document.getElementById("navBarFixed");
 const body = document.body;
 
 window.onscroll = function () {
-	myFunction();
-}
+  myFunction();
+};
 
 let sticky = navBarFixed.offsetTop;
 
 function myFunction() {
-	console.log(sticky)
-	if (window.pageYOffset >= sticky) {
-		navBarFixed.classList.add('sticky')
-	} else {
-		navBarFixed.classList.remove('sticky')
-	}
+  console.log(sticky);
+  if (window.pageYOffset >= sticky) {
+    navBarFixed.classList.add("sticky");
+  } else {
+    navBarFixed.classList.remove("sticky");
+  }
 }
 // fin nav bar fixed
 
@@ -24,25 +23,15 @@ function myFunction() {
 const dropdownBtn = document.querySelectorAll(".dropdown-button");
 const dropdownMenu = document.querySelectorAll(".dropdown-menu-content");
 for (let i = 0; i < dropdownBtn.length; i++) {
-	dropdownBtn[i].addEventListener("click", function () {
-		dropdownMenu[i].classList.toggle("visible");
-	})
+  dropdownBtn[i].addEventListener("click", function () {
+    dropdownMenu[i].classList.toggle("visible");
+  });
 }
 const dropdownImg = document.querySelectorAll(".card-img");
 for (let i = 0; i < dropdownBtn.length; i++) {
-	dropdownBtn[i].addEventListener("click", function () {
-		dropdownImg[i].classList.toggle("invisible");
-	})
+  dropdownBtn[i].addEventListener("click", function () {
+    dropdownImg[i].classList.toggle("invisible");
+  });
 }
 
-
 //fin retournement image pour texte
-
-
-
-
-
-
-
-
-
